3a79e407f7013cc72c662c35ef78f3bd58dcf920
Valeria <valerija.kozlova@tktk.ee>
2024-05-13 11:56:44 +0300

    week 1 code

---
+++ server.js
@@ -0,0 +1,8 @@
+let firstName = 'Harry';
+let lastName = 'Potter';
+//template literals
+// console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
+
